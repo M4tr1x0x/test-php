70bf991feb7f8c4960b8431010d5067df62e37ff
M4tr1x0x <alexgzzf@hotmail.com>
2024-09-26 22:45:44 -0600

    aa

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,19 +6,16 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    // Devuelve la vista index.blade.php
     public function index()
     {
         return view('products.index');
     }
 
-    // Devuelve la vista create.blade.php
     public function create()
     {
         return view('products.create');
     }
 
-    // No se mostrará, pero lo mantenemos aquí
     public function edit($id)
     {
         return view('products.edit');
